018ab7cf95228ae599cfa62d32e55123cd9194c8
takuma10Mi <takumaii@yahoo.co.jp>
2020-03-10 21:51:04 +0900

    ほぼ完了？

--- db/migrate/20200309112326_create_contacts.rb
+++ db/migrate/20200309112326_create_contacts.rb
@@ -3,7 +3,7 @@ class CreateContacts < ActiveRecord::Migration[5.2]
     create_table :contacts do |t|
       t.string :name
       t.string :mail
-      t.integer :phone
+      t.string :phone
       t.text :content
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2020_03_09_112326) do
   create_table "contacts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "mail"
-    t.integer "phone"
+    t.string "phone"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
