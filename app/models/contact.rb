698b012f5af68b2c8aa2e586a42fa64cb19f8e25
takuma10Mi <takumaii@yahoo.co.jp>
2020-03-10 21:35:00 +0900

    validation おｋ

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,13 @@
 class Contact < ApplicationRecord
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :mail, {presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
+
+  validates :name,
+    length: { minimum: 1, maximum: 10 };
+
+  validates :phone,
+  # phoneは数字のみ許可。ハイフンやカッコは認めない
+  numericality: { only_integer: true};
+  validates :content, presence: true;
 end
