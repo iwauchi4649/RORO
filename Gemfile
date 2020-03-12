4e43d189eaca02e73a1f91414cba313c6ceb9b54
takuma10Mi <takumaii@yahoo.co.jp>
2020-03-12 21:56:19 +0900

    reCAPTCHAの実装

--- Gemfile
+++ Gemfile
@@ -64,3 +64,5 @@ gem "haml-rails"
 gem "erb2haml"
 gem "gretel"
 gem 'dotenv-rails'
+gem 'recaptcha', require: "recaptcha/rails"
+gem 'rails-i18n'
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,13 +1,13 @@
 class Contact < ApplicationRecord
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :mail, {presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+validates :mail, {presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
 
-  validates :name,
-    length: { minimum: 1, maximum: 10 };
+validates :name,
+  length: { minimum: 1, maximum: 10 };
 
-  validates :phone,
-  # phoneは数字のみ許可。ハイフンやカッコは認めない
-  numericality: { only_integer: true};
-  validates :content, presence: true;
+VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
+validates :phone, presence: true, format: { with: VALID_PHONE_REGEX }
+  
+validates :content, presence: true;
 end
