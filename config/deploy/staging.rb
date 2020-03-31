c96b9b21970da2ccc02af20f3e5351d0ee972489
Syoma Iwauchi <syoma4645@gmail.com>
2020-03-31 13:03:55 +0900

    deploy delete

--- config/deploy/staging.rb
+++
@@ -1,7 +0,0 @@
-# conohaのサーバーのIP、ログインするユーザー名、サーバーの役割
-# xxxの部分はサーバーのIPアドレス
-# 10022はポートを変更している場合。通常は22
-server '133.167.65.60', user: 'syoma', roles: %w{app db web}, port: 61203
-
-#デプロイするサーバーにsshログインする鍵の情報。サーバー編で作成した鍵のパス
-set :ssh_options, keys: '~/.ssh/id_rsa'
